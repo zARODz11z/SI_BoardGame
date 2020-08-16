c875c3a78db1852c5d80010b5e6a32f2b2a0d53d
zARODz11z <49878080+zARODz11z@users.noreply.github.com>
2020-08-15 22:12:42 -0700

    updated input system

--- Assets/Scripts/boardGameManger.cs
+++ Assets/Scripts/boardGameManger.cs
@@ -6,18 +6,28 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Runtime.CompilerServices;
+using System;
 
 public class boardGameManger : MonoBehaviour
      
 {
     public GameObject dice;
-    public GameObject question;
+    //public string question;
+   // public string answer;
     public GameObject editQuestions;
     public Button displayQuestion, inputQuestion;
-    public TMP_InputField InputQuestion;
-    public TMP_InputField InputAnswer;
+    public InputField InputQuestion;
+    public InputField InputAnswer;
     public Text consoleText;
+    public int current_question;
+    public Text current_question_text;
+    
+    public Dictionary<int, string[]> game_data = new Dictionary<int, string[]>();
 
+    public void Start()
+    {
+        consoleText.text = "Successfully created " + DBManager.gameName + " Dont forget to save!";
+    }
 
 
     [DllImport("__Internal")]
@@ -32,25 +42,95 @@ public class boardGameManger : MonoBehaviour
 
     public void EditQuestions()
     {
-        editQuestions.SetActive(true);
+        consoleText.text = "";
         string button_name = EventSystem.current.currentSelectedGameObject.name;
-        Debug.Log(int.Parse(button_name) + 2);
+        current_question = int.Parse(button_name);
+        editQuestions.SetActive(true); //LEFT OFF HERE on 8/15
+        current_question_text.text = "Editing: " + current_question.ToString();
+        
+        if (game_data.ContainsKey(current_question))
+        {
+            InputQuestion.text = game_data[current_question][0];
+            InputAnswer.text = game_data[current_question][1];
+
+        }
+        //Debug.Log(int.Parse(button_name) + 2);
         //displayQuestion.interactable = false;
         //inputQuestion.interactable = true;
         //dice.SetActive(false);
 
     }
 
-    // Start is called before the first frame update
-    public void changeToQuestion()
+    public void confirm_question()
     {
-        dice.SetActive(false);
-        question.SetActive(true);
+        if (game_data.ContainsKey(current_question))
+        {
+            game_data.Remove(current_question);
+        }
+        string question = InputQuestion.text;
+        string answer = InputAnswer.text;
+        string[] Q_A_pair = { question, answer };
+
+        game_data.Add(current_question, Q_A_pair);
+        Debug.Log("Question: "+game_data[current_question][0]+ "\nAnswer: "+game_data[current_question][1]);
+        
+
+
     }
-    public void changeToDice()
+
+    public void next_question()
     {
-        Debug.Log("changedToDice");
-        dice.SetActive(true);
-        question.SetActive(false);
+        if (current_question < 31)
+        {
+            current_question += 1;
+            current_question_text.text = "Editing: " + current_question.ToString();
+            if (game_data.ContainsKey(current_question))
+            {
+                InputQuestion.text = game_data[current_question][0];
+                InputAnswer.text = game_data[current_question][1];
+
+            }
+            else
+            {
+                InputQuestion.text = "";
+                InputAnswer.text = "";
+            }
+        }
+    }
+
+    public void previous_question()
+    {
+        if (current_question > 0)
+        {
+            current_question -= 1;
+            current_question_text.text = "Editing: " + current_question.ToString();
+            if (game_data.ContainsKey(current_question))
+            {
+                InputQuestion.text = game_data[current_question][0];
+                InputAnswer.text = game_data[current_question][1];
+
+            }
+            else
+            {
+                InputQuestion.text = "";
+                InputAnswer.text = "";
+            }
+        }
+        
+    }
+
+    public void print_game_data()
+    {
+        foreach (KeyValuePair<int, string[]> kvp in game_data)
+        {
+            Debug.Log("Key: " + kvp.Key.ToString() + "\nQuestion: " + kvp.Value[0] + "\nAnswer: " + kvp.Value[1]);
+        }
+    }
+
+    public void close_edit_questions()
+    {
+        editQuestions.SetActive(false); //LEFT OFF HERE on 8/15
+        consoleText.text = "Dont forget to save your changes!";
+
     }
 }
